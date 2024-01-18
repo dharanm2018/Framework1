a0d4978bb73035ba0299605f2c4f8e0ceb7257ac
dharanm2018 <dharanm2018@gmail.com>
2024-01-18 21:01:52 +0530

    added page class for selectHotel

---
+++ src/test/java/com/pageObject/SelectHotelPage.java
@@ -0,0 +1,37 @@
+package com.pageObject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SelectHotelPage {
+	
+	private WebDriver driver;
+	
+	public SelectHotelPage() {
+		PageFactory.initElements(driver, this);
+	}
+@FindBy(id="radiobutton_0")
+private WebElement selectRadioButton;
+
+@FindBy(id="continue")
+private WebElement continueButton;
+
+public WebElement getSelectRadioButton() {
+	return selectRadioButton;
+}
+
+public WebElement getContinueButton() {
+	return continueButton;
+}
+	
+public void selectHotel() {
+	getSelectRadioButton().click();
+	getContinueButton().click();
+}
+
+
+
+	
+}
